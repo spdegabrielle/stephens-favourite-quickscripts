fb6e01d9d29fbe95fb56f41e947dbf510a8742e5
Stephen De Gabrielle <spdegabrielle@users.noreply.github.com>
2021-01-10 14:58:41 +0000

    new

---
+++ info.rkt
@@ -0,0 +1,21 @@
+#lang info
+(define collection "stephens-favourite-quuickscripts")
+(define pkg-desc "Mostly other peoples scripts")
+(define version "0.0")
+(define pkg-authors '(spdegabrielle))
+(define deps '("data-lib"
+               "base"
+               "drracket"
+               "gui-lib"
+               "htdp-lib"
+               "markdown"
+               "net-lib"
+               "plot-gui-lib"
+               "plot-lib"
+               "quickscript"
+               "rackunit-lib"
+               "scribble-lib"
+               "search-list-box"
+               "syntax-color-lib"))
+(define scribblings '(("scribblings/stephens-favourite-quuickscripts.scrbl" ())))
+(define build-deps '("at-exp-lib"))
