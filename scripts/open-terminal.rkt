1c2a6914dfe3e70e305d98a52cbd81f1ac707f78
Stephen De Gabrielle <spdegabrielle@users.noreply.github.com>
2021-01-10 15:01:13 +0000

    Create open-terminal.rkt

---
+++ scripts/open-terminal.rkt
@@ -0,0 +1,34 @@
+#lang racket/base
+(require racket/system
+         racket/path
+         quickscript)
+
+(script-help-string "Open a terminal in the current directory.")
+
+(define-script open-terminal
+  #:label "Open terminal here"
+  #:menu-path ("&Utils")
+  #:os-types (unix macosx windows)
+  #:output-to message-box
+
+  (case (system-type 'os)
+    [(unix)
+     (λ (str #:file f)  
+       (define dir (path->string (path-only f)))
+       (system (string-append "gnome-terminal"
+                              " --working-directory=\"" dir "\""
+                              " -t \"" dir "\""
+                              "&"))
+       #f)]
+    [(macosx)
+     (λ (str #:file f)  
+       (define dir (path->string (path-only f)))
+       (system
+        (string-append "osascript -e 'tell app \"Terminal\" to do script \"cd \\\"" dir "\\\"\"'" ))
+       #f)]
+    [(windows)
+     (λ (str #:file f)
+       (define dir (path->string (path-only f)))
+       (shell-execute #f "cmd.exe" "" dir 'sw_shownormal))]))
+       
+       
